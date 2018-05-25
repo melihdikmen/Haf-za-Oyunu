b8c08eb54c57bb2b08b5bd2469c80e21ca02c90b
Melih <melihdikmen>
2018-05-25 14:17:24 +0300

    hafıza oyunu

---
+++ Hafıza Oyunu/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Hafıza_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "1.jpg");
+            this.ımageList1.Images.SetKeyName(1, "2.jpg");
+            this.ımageList1.Images.SetKeyName(2, "3.png");
+            this.ımageList1.Images.SetKeyName(3, "4.jpg");
+            this.ımageList1.Images.SetKeyName(4, "5.jpg");
+            this.ımageList1.Images.SetKeyName(5, "6.jpg");
+            this.ımageList1.Images.SetKeyName(6, "7.jpg");
+            this.ımageList1.Images.SetKeyName(7, "8.png");
+            this.ımageList1.Images.SetKeyName(8, "9.jpg");
+            this.ımageList1.Images.SetKeyName(9, "10.jpg");
+            this.ımageList1.Images.SetKeyName(10, "11.jpg");
+            this.ımageList1.Images.SetKeyName(11, "12.jpg");
+            this.ımageList1.Images.SetKeyName(12, "13.jpg");
+            this.ımageList1.Images.SetKeyName(13, "14.jpg");
+            this.ımageList1.Images.SetKeyName(14, "15.jpg");
+            this.ımageList1.Images.SetKeyName(15, "16.png");
+            this.ımageList1.Images.SetKeyName(16, "17.jpg");
+            this.ımageList1.Images.SetKeyName(17, "18.jpg");
+            this.ımageList1.Images.SetKeyName(18, "19.jpg");
+            this.ımageList1.Images.SetKeyName(19, "20.png");
+            this.ımageList1.Images.SetKeyName(20, "1.jpg");
+            this.ımageList1.Images.SetKeyName(21, "2.jpg");
+            this.ımageList1.Images.SetKeyName(22, "3.png");
+            this.ımageList1.Images.SetKeyName(23, "4.jpg");
+            this.ımageList1.Images.SetKeyName(24, "5.jpg");
+            this.ımageList1.Images.SetKeyName(25, "6.jpg");
+            this.ımageList1.Images.SetKeyName(26, "7.jpg");
+            this.ımageList1.Images.SetKeyName(27, "8.png");
+            this.ımageList1.Images.SetKeyName(28, "9.jpg");
+            this.ımageList1.Images.SetKeyName(29, "10.jpg");
+            this.ımageList1.Images.SetKeyName(30, "11.jpg");
+            this.ımageList1.Images.SetKeyName(31, "12.jpg");
+            this.ımageList1.Images.SetKeyName(32, "13.jpg");
+            this.ımageList1.Images.SetKeyName(33, "14.jpg");
+            this.ımageList1.Images.SetKeyName(34, "15.jpg");
+            this.ımageList1.Images.SetKeyName(35, "16.png");
+            this.ımageList1.Images.SetKeyName(36, "17.jpg");
+            this.ımageList1.Images.SetKeyName(37, "18.jpg");
+            this.ımageList1.Images.SetKeyName(38, "19.jpg");
+            this.ımageList1.Images.SetKeyName(39, "20.png");
+            this.ımageList1.Images.SetKeyName(40, "21.png");
+            this.ımageList1.Images.SetKeyName(41, "22.png");
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(17, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sıra:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(57, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "none";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 247);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 262);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Doğru Sayıları";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "1.Oyuncu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "2.Oyuncu:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(95, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(95, 89);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 45);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Yeniden Başlat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(17, 146);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Süre:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(63, 146);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 20);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 582);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Hafıza Oyunu/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Hafıza_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            olustur();
+            timer1.Start();
+        }
+
+
+        Random rnd = new Random();
+        int[] kontrol = new int[40];
+        PictureBox[,] pctrdizi = new PictureBox[5,8];
+        int oyuncu = 0;
+        int dogru1 = 0;
+        int dogru2 = 0;
+        public void olustur()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    PictureBox pctr = new PictureBox();
+                    pctr.Width = 100;
+                    pctr.Top = 50 + (i * 100) ;
+                    pctr.Left = 400 + (j*125);
+                    pctrdizi[i, j] = pctr;
+
+                    pctr.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pctr.Click += new EventHandler(karşılaştırma);
+                   
+                    int rast = rnd.Next(0, 40);
+                    
+                    if (kontrol[rast] == 1)
+                   
+                    {
+
+                        j--;
+                        
+
+
+                    }
+
+                    else
+                    {
+                        pctrdizi[i, j].Image = ımageList1.Images[rast];
+                        kontrol[rast] += 1;
+                        pctrdizi[i, j].Tag = rast; 
+                        ımageList1.Images[rast].Tag=rast;
+                        this.Controls.Add(pctr);
+                    }
+
+                    
+                   
+
+                }
+            }
+
+            oyuncu = 1;
+            label2.Text = oyuncu + ".oyuncu";
+
+           
+
+        }
+
+        
+        
+        
+        int sure = 5;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (sure == 0)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        pctrdizi[i, j].Image = ımageList1.Images[40];
+                    }
+                }
+
+                timer1.Stop();
+                sure = 5;
+                label8.Text = sure.ToString();
+
+            }
+
+            else
+            {
+                sure -= 1;
+                label8.Text = sure.ToString();
+            }
+        }
+
+
+        int sayac = 0;
+        int[] pc = new int[2];
+        
+        public void karşılaştırma(object sender, EventArgs e)
+        {
+            PictureBox picture = (PictureBox)sender;
+
+            if (sayac < 2)
+            {
+                int indeks = (int)picture.Tag;
+                picture.Image = ımageList1.Images[indeks];
+                pc[sayac] = indeks;
+                sayac++;
+                timer2.Start();
+            }
+        }
+
+
+
+      
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+           
+            if (sure == 0)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        
+                        if(pctrdizi[i,j].Tag != "dogru")
+                           {
+                               pctrdizi[i,j].Image = ımageList1.Images[40];
+                           }
+                        
+                    }
+                }
+                sure = 5;
+                label8.Text = sure.ToString();
+                sayac = 0;
+                timer2.Stop();
+
+                if (oyuncu == 1)
+                {
+                    oyuncu = 2;
+                    label2.Text = oyuncu + ".oyuncu";
+
+                }
+
+                else
+                {
+                    oyuncu = 1;
+                    label2.Text = oyuncu + ".oyuncu";
+                }
+            }
+
+            else
+            {
+                sure--;
+                label8.Text = sure.ToString();
+            }
+
+
+
+
+            if (pc[0] == pc[1] + 20 || pc[0] + 20 == pc[1])
+            {
+
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (pctrdizi[i, j].Tag.ToString() == pc[0].ToString() || pctrdizi[i, j].Tag.ToString() == pc[1].ToString())
+                        {
+                            pctrdizi[i, j].Image = ımageList1.Images[41];
+                            pctrdizi[i, j].Tag = "dogru";
+                        }
+
+                    }
+                }
+                sure = 5;
+                label8.Text = sure.ToString();
+                sayac = 0;
+                timer2.Stop();
+                if (oyuncu == 1)
+                {
+                    dogru1 += 1;
+                    label5.Text =dogru1.ToString();
+
+                }
+
+                else
+                {
+                    dogru2 += 1;
+                    label6.Text = dogru2.ToString();
+                }
+
+            }
+
+
+            else
+            {
+                if (sayac == 2)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        for (int j = 0; j < 8; j++)
+                        {
+                            if (pctrdizi[i, j].Tag != "dogru")
+                            {
+                                pctrdizi[i, j].Image = ımageList1.Images[40];
+                            }
+                        }
+                    }
+
+                    sure = 5;
+                    label8.Text = sure.ToString();
+                    sayac = 0;
+                    timer2.Stop();
+                    if (oyuncu == 1)
+                    {
+                        oyuncu = 2;
+                        label2.Text = oyuncu + ".oyuncu";
+
+                    }
+
+                    else
+                    {
+                        oyuncu = 1;
+                        label2.Text = oyuncu + ".oyuncu";
+                    }
+                }
+            }
+
+
+
+
+            if (dogru1 == 11)
+            {
+                MessageBox.Show("1.Oyuncu Kazanmıştır.");
+            }
+
+            if (dogru2 == 11)
+            {
+                MessageBox.Show("1.Oyuncu Kazanmıştır.");
+            }
+
+              if(dogru1==10&&dogru2==10)
+            {
+                MessageBox.Show("Berabere!!!");   
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+       
+    }
+
+
+
+
+}
+
+
